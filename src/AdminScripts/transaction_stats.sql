3c17604f64094809169940caf8555699201de821
Zachary Christopherson <chriz@amazon.com>
2015-08-06 11:39:47 -0700

    Transaction timings and sizes

---
+++ src/AdminScripts/transaction_stats.sql
@@ -0,0 +1,36 @@
+WITH 
+	xid_blocks AS
+		(SELECT cs.xid
+			,SUM(cs.newblocks) newblocks
+			,SUM(cs.dirtyblocks) dirtyblocks
+			,SUM(cs.headers) headers
+		FROM
+			stl_commit_stats cs
+		WHERE (1=1)
+			AND cs.xid > 0
+			AND cs.endtime > cs.startqueue
+		GROUP BY cs.xid)
+	xids AS 
+		(SELECT xid
+		,split_part(lower(trim(replace(replace(s.text,';',''),':',''))),' ',1) cmd
+		,s.starttime cmd_starttime
+		,s.endtime cmd_endtime
+		,cs.startqueue 
+		,cs.startwork
+		,cs.endtime commit_endtime
+		,cs.queuelen
+		,xb.newblocks
+		,xb.dirtyblocks
+		,xb.headers
+		,datediff(ms,cs.startqueue,cs.startwork) commit_queue_dur_ms
+		,datediff(ms,cs.startqueue,cs.endtime) total_commit_dur_ms
+		,datediff(ms,cs.startwork,cs.endtime) commit_work_dur_ms
+		,rank () over (partition by xid order by s.starttime) as rank
+	FROM svl_statementtext s
+	JOIN stl_commit_stats cs USING (xid)
+	JOIN xid_blocks xb USING (xid)
+	WHERE (1=1)
+		AND s.sequence=0
+		AND cs.node = -1
+		AND cs.xid > 0
+		AND cs.endtime > cs.startqueue)
